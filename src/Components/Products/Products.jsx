dda832b69efadf4b3d1b530f0b73cce59d07a498
DanaAyoub <danaayoub99@hotmail.com>
2021-08-10 10:25:42 +0300

    last edit

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Products.scss';
 import '../../Components/Card/Card.scss';
 import Card from '../../Components/Card/Card.jsx';
